4a9f0fe20c85ee0e2bcddaa7c0f9f867ec0b1875
rishavhav <rishavsoammw123@gmail.com>
2022-10-09 18:48:29 -0400

    assignment 6

--- assignments/assignment6/my_KMeans.py
+++ assignments/assignment6/my_KMeans.py
@@ -7,7 +7,6 @@ class my_KMeans:
         # init = {"k-means++", "random"}
         # stop when either # iteration is greater than max_iter or the delta of self.inertia_ is smaller than tol.
         # repeat n_init times and keep the best run (cluster_centers_, inertia_) with the lowest inertia_.
-
         self.n_clusters = int(n_clusters)
         self.init = init
         self.n_init = n_init
@@ -20,25 +19,99 @@ class my_KMeans:
         # Sum of squared distances of samples to their closest cluster center.
         self.inertia_ = None
 
+    def dist(self, a, b):
+        # Compute Euclidean distance between a and b
+        return np.sum((np.array(a)-np.array(b))**2)**(0.5)
+
+    def initiate(self, X):
+        # Initiate cluster centers
+        # Input X is numpy.array
+        # Output cluster_centers (list)
+        if self.init == "random":
+
+            cluster_centers = X[np.random.choice(range(len(X)))]
+
+        elif self.init == "k-means++":
+
+            
+            cluster_centers = [X[0]]
+            
+            for _ in range(self.n_clusters-1):
+                # Calculate distances from points to the centroids
+              dists = np.array([min([np.inner(center-x,center-x) for center in cluster_centers]) for x in X])
+              cumulative_probs = (dists/dists.sum()).cumsum()
+              r_var = np.random.rand()
+
+              for k, v in enumerate(cumulative_probs):
+                  if r_var < v:
+                      i = k
+                      break
+            
+              cluster_centers.append(X[i])
+        else:
+            raise Exception("Unknown value of self.init.")
+        return cluster_centers
+
+    def fit_once(self, X):
+        # Fit once
+        # Input X is numpy.array
+        # Output: cluster_centers (list), inertia
+
+        # Initiate cluster centers
+        cluster_centers = self.initiate(X)
+        last_inertia = None
+        # Iterate
+        for i in range(self.max_iter+1):
+            # Assign each training data point to its nearest cluster_centers
+            clusters = [[] for i in range(self.n_clusters)]
+            inertia = 0
+            for x in X:
+                # calculate distances between x and each cluster center
+                dists = [self.dist(x, center) for center in cluster_centers]
+                # calculate inertia
+                inertia += np.sum(np.square(np.amin(dists)))
+                # find the cluster that x belongs to
+                cluster_id = np.argmin(dists)
+                # add x to that cluster
+                clusters[cluster_id].append(x)
+
+            if (last_inertia and last_inertia - inertia < self.tol) or i==self.max_iter:
+                break
+            # Update cluster centers
+
+            cluster_centers = [np.mean(cluster, axis=0) for cluster in clusters]
+
+            last_inertia = inertia
+
+        return cluster_centers, inertia
+
+
     def fit(self, X):
-        # X: pd.DataFrame, independent variables, float        
-        # repeat self.n_init times and keep the best run 
+        # X: pd.DataFrame, independent variables, float
+        # repeat self.n_init times and keep the best run
             # (self.cluster_centers_, self.inertia_) with the lowest self.inertia_.
-        # write your code below
+        X_feature = X.to_numpy()
+        for i in range(self.n_init):
+            cluster_centers, inertia = self.fit_once(X_feature)
+            if self.inertia_==None or inertia < self.inertia_:
+                self.inertia_ = inertia
+                self.cluster_centers_ = cluster_centers
         return
 
-    def predict(self, X):
-        # X: pd.DataFrame, independent variables, float
-        # return predictions: list
-        # write your code below
-        return predictions
 
     def transform(self, X):
         # Transform to cluster-distance space
         # X: pd.DataFrame, independent variables, float
         # return dists = list of [dist to centroid 1, dist to centroid 2, ...]
-        # write your code below
+        dists = [[self.dist(x,centroid) for centroid in self.cluster_centers_] for x in X.to_numpy()]
         return dists
+
+    def predict(self, X):
+        # X: pd.DataFrame, independent variables, float
+        # return predictions: list
+        predictions = [np.argmin(dist) for dist in self.transform(X)]
+        return predictions
+
 
     def fit_predict(self, X):
         self.fit(X)
@@ -47,6 +120,7 @@ class my_KMeans:
     def fit_transform(self, X):
         self.fit(X)
         return self.transform(X)
+
 
 
 
